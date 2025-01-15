e89eeadeb17287da42527e0024820bc312410baa
Dwitikrishna Parida <krishnaparida011@gmail.com>
2025-01-15 20:22:19 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaskInput from './Components/TaskInput'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <TaskInput />
+    </div>
+  )
+}
+
+export default App
